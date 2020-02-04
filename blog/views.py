7afd3b066797a854c9b338c76c6329d866e2542e
Joshua Hwang <jhwang@solverglobal.com>
2020-02-03 20:52:33 -0500

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Josh H.',
+        'title': 'Blog Post 1',
+        'content': 'First Post!',
+        'date_posted': '1/22/2020'
+    },
+    {
+        'author': 'Josh H.',
+        'title': 'Blog Post 2',
+        'content': 'Second Post!',
+        'date_posted': '1/22/2020'
+    }
+]
+
+# Create your views here.
+def home(request):
+    context = {
+        '[p]'
+    }
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
